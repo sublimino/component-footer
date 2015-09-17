f7532347e8f76af908d4555d9ab47f52a4cf8bf1
GoCD <ecprod@economist.com>
2015-09-17 11:32:50 +0000

    ghpages

---
+++ example.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.__esModule = true;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _context = require('./context');
+
+var _context2 = _interopRequireDefault(_context);
+
+var _indexEs6 = require('./index.es6');
+
+var _indexEs62 = _interopRequireDefault(_indexEs6);
+
+exports['default'] = _react2['default'].createElement(_indexEs62['default'], { data: _context2['default'] });
+module.exports = exports['default'];
